ff86579bf5db9baf3fab8ad811fd30d90dddec56
Hangarter <madison.paz@gmail.com>
2023-08-09 01:33:01 +0200

    wip

--- pe-frontend/src/components/BlocklyEditor.tsx
+++ pe-frontend/src/components/BlocklyEditor.tsx
@@ -4,36 +4,73 @@ import Blockly from "blockly";
 
 const BlocklyEditor: React.FC = () => {
   const toolbox = {
-    kind: "flyoutToolbox",
+    kind: "categoryToolbox",
     contents: [
       {
-        kind: "block",
-        type: "controls_if",
+        kind: "category",
+        name: "Logic",
+        colour: "210",
+        contents: [
+          {
+            kind: "block",
+            type: "controls_if",
+          },
+          {
+            kind: "block",
+            type: "controls_repeat_ext",
+          },
+          {
+            kind: "block",
+            type: "logic_compare",
+          },
+          {
+            kind: "block",
+            type: "math_number",
+          },
+          {
+            kind: "block",
+            type: "variables_get_dynamic",
+          },
+        ],
       },
       {
-        kind: "block",
-        type: "controls_repeat_ext",
+        kind: "sep",
       },
       {
-        kind: "block",
-        type: "logic_compare",
+        kind: "category",
+        name: "Values",
+        colour: "120",
+        contents: [
+          {
+            kind: "block",
+            type: "logic_boolean",
+          },
+          {
+            kind: "block",
+            type: "text_join",
+          },
+          {
+            kind: "block",
+            type: "variables_set",
+          },
+          {
+            kind: "block",
+            type: "variables_get",
+          },
+        ],
       },
-      {
-        kind: "block",
-        type: "math_number",
-      },
-      {
-        kind: "block",
-        type: "math_arithmetic",
-      },
-      {
-        kind: "block",
-        type: "text",
-      },
-      {
-        kind: "block",
-        type: "text_print",
-      },
+
+      //   {
+      //     kind: "category",
+      //     name: "Variables",
+      //     custom: "VARIABLE",
+      //     colour: "320",
+      //   },
+      //   {
+      //     kind: "category",
+      //     name: "Functions",
+      //     custom: "PROCEDURE",
+      //   },
     ],
   };
 
@@ -41,9 +78,61 @@ const BlocklyEditor: React.FC = () => {
 
   useEffect(() => {
     if (workspace === null) {
+      //   Blockly.defineBlocksWithJsonArray([
+      //     {
+      //       type: "variables_set",
+      //       message0: "%{BKY_VARIABLES_SET}",
+      //       args0: [
+      //         {
+      //           type: "field_variable",
+      //           name: "VAR",
+      //           variable: "%{BKY_VARIABLES_DEFAULT_NAME}",
+      //         },
+      //         {
+      //           type: "input_value", // This expects an input of any type
+      //           name: "VALUE",
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       type: "string_length",
+      //       message0: "length of %1",
+      //       args0: [
+      //         {
+      //           type: "input_value",
+      //           name: "VALUE",
+      //           check: "String",
+      //         },
+      //       ],
+      //       output: "Number",
+      //       colour: 160,
+      //       tooltip: "Returns number of letters in the provided text.",
+      //       helpUrl: "http://www.w3schools.com/jsref/jsref_length_string.asp",
+      //     },
+      //   ]);
       workspace = Blockly.inject("blocklyDiv", {
         toolbox: toolbox,
+        horizontalLayout: false,
+        grid: { spacing: 20, length: 3, colour: "#ccc", snap: true },
+        zoom: {
+          controls: true,
+          wheel: true,
+          startScale: 1.0,
+          maxScale: 3,
+          minScale: 0.3,
+          scaleSpeed: 1.2,
+          pinch: true,
+        },
+        move: {
+          scrollbars: {
+            horizontal: true,
+            vertical: true,
+          },
+          drag: true,
+          wheel: false,
+        },
       });
+      workspace.createVariable("GL_MANUFACTURER");
     }
   }, []);
 
